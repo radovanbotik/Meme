562d57ae7811bb799410e7aab63f6b3a98314e0e
radovanbotik <radovanbotik@gmail.com>
2022-10-31 18:06:38 +0100

    ''

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,35 +6,49 @@ export default function Body() {
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+
+  const [lines, setLines] = useState({
+    linetop: "",
+    linebottom: "",
+  });
+
   const [allMemeImages, setAllMemeImages] = useState(memeData.data.memes);
   const jokes = memeData.data.memes;
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setLines(prev => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = () => {};
+
   const getJoke = e => {
     e.preventDefault();
-
     const randomNumber = () => {
       return Math.floor(Math.random() * jokes.length);
     };
-    // const randomJokeURL = () => {
-    //   return jokes[randomNumber()].url;
-    // };
     setMeme(prevState => {
       return { ...prevState, randomImage: allMemeImages[randomNumber()].url };
     });
   };
   return (
     <div className="appbody">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           className="line-1"
           placeholder="line-top"
           name="linetop"
+          onChange={handleChange}
+          value={lines.linetop}
         />
         <input
           type="text"
           className="line2"
           placeholder="line-bottom"
           name="linebottom"
+          onChange={handleChange}
+          value={lines.linebottom}
         />
         <button className="submit" id="submit" onClick={getJoke}>
           Get a new meme image
@@ -42,6 +56,8 @@ export default function Body() {
       </form>
       <div className="meme-final">
         <img src={meme.randomImage} alt="" />
+        <h2 className="line lineTop">{lines.linetop}</h2>
+        <h2 className="line lineBottom">{lines.linebottom}</h2>
       </div>
     </div>
   );
